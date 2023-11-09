5baad3e5a66df83a3cec0ec3a8ed6d4dd6de62bb
yogitachavan5404 <60497983+yogitachavan5404@users.noreply.github.com>
2023-11-09 22:13:17 +0530

    Update PetStoreAPITestCases.cs
    Test methods added

--- PetStoreProject/PetStoreAPITestCases.cs
+++ PetStoreProject/PetStoreAPITestCases.cs
@@ -1,22 +1,53 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using System.Net;
 
 namespace PetStoreProject
 {
     [TestClass]
     public class PetStoreAPITestCases
+
     {
         [TestMethod]
-        public void Test()
+        public async Task VerifyOKResponseForDetailsOfPetAsync()
         {
             try
             {
-                HttpWebRequest hrrp = (HttpWebRequest)WebRequest.Create("https://petstore.swagger.io/v2/pet/5");
-                HttpWebResponse htt = (HttpWebResponse)hrrp.GetResponse();
-                Assert.AreEqual(HttpStatusCode.OK, htt.StatusCode);
+                using var client = new HttpClient();
+                var result = await client.GetAsync("https://petstore.swagger.io/v2/pet/5");
+                Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
             }
-            catch (Exception ex) 
+            catch (Exception ex)
+            {
+                Assert.Fail("Test Case failed beacuse of" + ex.StackTrace);
+            }
+        }
+        [TestMethod]
+        public async Task VerifyNotFoundResponseForNegativeEndPointAsync()
+        {
+            try
             {
 
+                using var client = new HttpClient();
+                var result = await client.GetAsync("https://petstore.swagger.io/v2/pet/-5");
+                Assert.AreEqual(HttpStatusCode.NotFound, result.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Test Case failed beacuse of" + ex.StackTrace);
+            }
+        }
+        [TestMethod]
+        public async Task VerifyNotAllowedResponseForBlankEndPointAsync()
+        {
+            try
+            {
+                using var client = new HttpClient();
+                var result = await client.GetAsync("https://petstore.swagger.io/v2/pet/");
+                Assert.AreEqual(HttpStatusCode.MethodNotAllowed, result.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Test Case failed beacuse of" + ex.StackTrace);
             }
         }
     }
